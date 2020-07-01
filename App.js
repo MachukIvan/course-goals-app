955a09784d3639dd2d853a8dd41d83b2e6500a53
Ivan Machuk <machuk.ivan@gmail.com>
2020-07-02 01:13:55 +0300

    Added cancel button, set up styling

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
     setCourseGoals(currentGoals => [
       ...currentGoals,
       { value: goalTitle, id: Math.random().toString() },
     ]);
+    setIsAddMode(false);
   };
 
   const deleteGoalHandler = goalID => {
@@ -20,9 +22,18 @@ export default function App() {
     });
   };
 
+  const cancelGoalEditHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        onAddGoal={addGoalHandler}
+        visible={isAddMode}
+        onCancel={cancelGoalEditHandler}
+      />
       <FlatList
         keyExtractor={item => item.id}
         data={courseGoals}
